0cfdfacef2cd059c3b2ff18d64e0dee1ed38bb9c
Tomohisa Maruyama <tomohisa.maruyama@astrazeneca.com>
2024-03-14 19:07:39 +0900

    Initial commit

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+# Install Docker
+```
+curl -fsSL https://raw.githubusercontent.com/tmarumt/dotfiles/main/scripts/install_docker_wsl_ubuntu.sh | sh
+```
---
+++ scripts/install_docker_wsl_ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+sudo tee /etc/docker/daemon.json << EOL > /dev/null
+{
+  "ip": "127.0.0.1"
+}
+EOL
+
+sudo systemctl enable --now docker
